2893a93c2bb145cb4640563531d6609864b8725c
chiupin <chiupin.chen@hotmail.com>
2023-06-13 19:15:47 -0500

    added a new test

--- tests/addMethod.test.ts
+++ tests/addMethod.test.ts
@@ -5,3 +5,9 @@ describe('testing index file', () => {
     expect(add('')).toBe(0);
   });
 });
+
+describe('testing index file2', () => {
+  test('1,2,3,4 string should result in 10', () => {
+    expect(add('1,2,3,4')).toBe(10);
+  });
+});
